2321f67f40173d401bcc82d6cde7e41c9ed93746
Stephen Tollafield <STollafield@beraninstruments.com.local>
2021-02-19 13:02:19 +0000

    Stuff

---
+++ Text Based Adventure Game/Assets/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "You awake . Your phone is gone";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
